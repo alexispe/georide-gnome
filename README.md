4df8d9d10d03cea1883da7d0f96a42b41f787a6e
alexisp <contact@alexis-petit.fr>
2019-03-31 23:26:26 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# GeoRide Gnome
+![Logo GeoBobo](https://raw.githubusercontent.com/alexispe/georide-gnome/master/georide-logo.png)
+
+> V à tous les motards de France et de Navarre utilisateurs de Gnome et possesseurs de GeoRide.
+> Extension non officielle.
+
+Ceci est une incroyable extension GNOME Shell qui va te permettre de communiquer avec ta moto comme par magie :
+- Affiche l'état de vérrouillage de ton tracker dans la status bar
+- Affiche une incroyable popup te permettant de :
+  - Voir le nombre de km de la moto
+  - Localiser la moto sur google maps
+  - Vérrouiller/Déverrouiller la moto
+
+![Demo Popup GeoBobo](https://raw.githubusercontent.com/alexispe/georide-gnome/master/georide-popup.png)
+
+## Installation
+```
+cd ~/.local/share/gnome-shell/extensions/
+git clone https://github.com/alexispe/georide-gnome.git GeoRide@frenchco.de
+```
+Ensuite vous devez modifier vos identifiants dans le fichier ```extension.js```
+```
+nano ~/.local/share/gnome-shell/extensions/GeoRide@frenchco.de/extension.js
+```
+Remplacez les champs :
+```
+const GEORIDE_EMAIL = 'grutier@gmail.com'
+const GEORIDE_PASSWORD = 'helicoptere'
+```
+Bravo, maintenant vous n'avez plus qu'à activer l'extension.
+Rendez vous dans l'interface de Gnome Tweak Tool (via la commande ```gnome-tweak-tool```) > onglet Extensions.
+Ou directement sur cette page (https://extensions.gnome.org/local/).
+Puis activez GeoRide.
+
+
+
+V
---
+++ extension.js
@@ -0,0 +1,194 @@
+#!/usr/bin/gjs
+
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Soup = imports.gi.Soup;
+const Lang = imports.lang;
+const Util = imports.misc.util;
+const Mainloop = imports.mainloop;
+const Clutter = imports.gi.Clutter;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+
+const GEORIDE_URL = 'https://api.georide.fr/';
+const GEORIDE_EMAIL = 'YOUR_EMAIL';
+const GEORIDE_PASSWORD = 'YOUR_PASSWORD';
+let GEORIDE_TOKEN;
+
+let _httpSession;
+let _apiTrackers;
+
+const GeoRideIndicator = new Lang.Class({
+  Name: 'GeoRideIndicator',
+  Extends: PanelMenu.Button,
+
+  _init: function () {
+    this.parent(0.0, "GeoRide", false);
+    this.buttonText = new St.Label({
+      text: _("Chargement..."),
+      y_align: Clutter.ActorAlign.CENTER
+    });
+    this.actor.add_actor(this.buttonText);
+
+    this.menuItem = new PopupMenu.PopupMenuItem('0km');
+    this.menuItemLocaliser = new PopupMenu.PopupMenuItem('Localiser');
+    this.switchmenuitem = new PopupMenu.PopupSwitchMenuItem('Vérrouillée',false);
+    // this.menuItem.label = new St.Label({ text: 'la bize' });
+    // this.menuItem.actor.add_child(this.menuItem.label);
+
+    this.menu.addMenuItem(this.menuItem);
+    this.menu.addMenuItem(this.menuItemLocaliser);
+    this.menu.addMenuItem(this.switchmenuitem);
+
+    this.menuItemLocaliser.actor.connect('button-press-event', Lang.bind(this, function() {
+      Util.spawnCommandLine("sensible-browser https://www.google.com/maps/search/?api=1&query="+_apiTrackers[0].latitude+","+_apiTrackers[0].longitude);
+    }));
+    this.switchmenuitem.connect('toggled', Lang.bind(this, function(object, value){
+      if(value) this._apiLockTracker();
+      else this._apiUnlockTracker();
+    }));
+
+    this._apiLogin();
+    this._refresh();
+    this._apiRegenerateToken();
+
+  },
+
+  _refresh: function () {
+    this._apiGetTrackers(this._refreshUI);
+    this._removeTimeout();
+    this._timeout = Mainloop.timeout_add_seconds(10, Lang.bind(this, this._refresh));
+    return true;
+  },
+
+  _refreshUI: function (data) {
+    // log('[GEORIDE_LOG]', JSON.stringify(data[0]));
+    let trackerName = data[0].trackerName.toString();
+    let status = data[0].status.toString();
+    let isLocked = data[0].isLocked ? 'Vérrouillée' : 'Déverouillée';
+    txt = trackerName + ' ' + isLocked;
+    this.buttonText.set_text(txt);
+    this.menuItem.actor.label_actor.set_text((data[0].odometer/1000)+'km');
+    if(data[0].isLocked) this.switchmenuitem.setToggleState(true);
+  },
+
+  _removeTimeout: function () {
+    if (this._timeout) {
+      Mainloop.source_remove(this._timeout);
+      this._timeout = null;
+    }
+  },
+
+  stop: function () {
+    if (_httpSession !== undefined)
+      _httpSession.abort();
+    _httpSession = undefined;
+
+    if (this._timeout)
+      Mainloop.source_remove(this._timeout);
+    this._timeout = undefined;
+
+    this.menu.removeAll();
+  },
+
+  _apiRegenerateToken: function () {
+    _httpSession = new Soup.Session();
+    let message = Soup.form_request_new_from_hash('GET', GEORIDE_URL+'user/new-token', {});
+    message.request_headers.append("Authorization", 'Bearer '+GEORIDE_TOKEN);
+    _httpSession.queue_message(message, Lang.bind(this, function (_httpSession, message) {
+      log('[GEORIDE_LOG]', message.response_body.data);
+          if (message.status_code !== 200) return;
+          let json = JSON.parse(message.response_body.data);
+          GEORIDE_TOKEN = json.authToken;
+        }
+      )
+    );
+  },
+
+  _apiLogin: function () {
+    log('[GEORIDE_LOG]', '_apiLogin');
+    _httpSession = new Soup.Session();
+    let message = Soup.form_request_new_from_hash('POST', GEORIDE_URL+'user/login', {
+      email: GEORIDE_EMAIL,
+      password: GEORIDE_PASSWORD
+    });
+    _httpSession.queue_message(message, Lang.bind(this, function (_httpSession, message) {
+      log('[GEORIDE_LOG]', message.response_body.data);
+      if (message.status_code !== 200) return;
+      let json = JSON.parse(message.response_body.data);
+      GEORIDE_TOKEN = json.authToken;
+    }));
+  },
+
+  _apiGetTrackers: function () {
+    _httpSession = new Soup.Session();
+    let message = Soup.form_request_new_from_hash('GET', GEORIDE_URL+'user/trackers', {});
+    message.request_headers.append("Authorization", 'Bearer '+GEORIDE_TOKEN);
+    _httpSession.queue_message(message, Lang.bind(this, function (_httpSession, message) {
+          if (message.status_code !== 200) return;
+          // log('[GEORIDE_LOG]', 'queue_message');
+          // log('[GEORIDE_LOG]', message.response_body.data);
+          let json = JSON.parse(message.response_body.data);
+          _apiTrackers = json;
+          this._refreshUI(json);
+        }
+      )
+    );
+  },
+
+  _apiToggleTracker: function () {
+    log('[GEORIDE_LOG]', '_apiToggleTracker');
+    // log('[GEORIDE_LOG]', _apiTrackers);
+    _httpSession = new Soup.Session();
+    let message = Soup.form_request_new_from_hash('POST', GEORIDE_URL+'tracker/'+_apiTrackers[0].trackerId+'/toggleLock', {});
+    message.request_headers.append("Authorization", 'Bearer '+GEORIDE_TOKEN);
+    _httpSession.queue_message(message, Lang.bind(this, function (_httpSession, message) {
+      log('[GEORIDE_LOG]', message.response_body.data);
+      if (message.status_code !== 200) return;
+      this._apiGetTrackers();
+      let json = JSON.parse(message.response_body.data);
+      if(json.locked) Main.notify(_apiTrackers[0].trackerName + ' Vérrouillée');
+      else  Main.notify(_apiTrackers[0].trackerName + ' Déverouillée')
+    }));
+  },
+
+  _apiLockTracker: function () {
+    // log('[GEORIDE_LOG]', '_apiLockTracker');
+    _httpSession = new Soup.Session();
+    let message = Soup.form_request_new_from_hash('POST', GEORIDE_URL+'tracker/'+_apiTrackers[0].trackerId+'/lock', {});
+    message.request_headers.append("Authorization", 'Bearer '+GEORIDE_TOKEN);
+    _httpSession.queue_message(message, Lang.bind(this, function (_httpSession, message) {
+      // log('[GEORIDE_LOG]', message.response_body.data);
+      this._apiGetTrackers();
+      Main.notify(_apiTrackers[0].trackerName + ' Vérrouillée');
+    }));
+  },
+
+  _apiUnlockTracker: function () {
+    // log('[GEORIDE_LOG]', '_apiLockTracker');
+    _httpSession = new Soup.Session();
+    let message = Soup.form_request_new_from_hash('POST', GEORIDE_URL+'tracker/'+_apiTrackers[0].trackerId+'/unlock', {});
+    message.request_headers.append("Authorization", 'Bearer '+GEORIDE_TOKEN);
+    _httpSession.queue_message(message, Lang.bind(this, function (_httpSession, message) {
+      // log('[GEORIDE_LOG]', message.response_body.data);
+      this._apiGetTrackers();
+      Main.notify(_apiTrackers[0].trackerName + ' Déverouillée');
+    }));
+  },
+
+
+});
+
+let twMenu;
+
+function init() {}
+
+function enable() {
+	twMenu = new GeoRideIndicator;
+	Main.panel.addToStatusArea('tw-indicator', twMenu);
+}
+
+function disable() {
+	twMenu.stop();
+	twMenu.destroy();
+}
